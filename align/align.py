c15d70f20468c353c2eef4f2b7bd95e26b114ab9
Varun Iyer <varun_iyer@posteo.net>
2019-08-29 16:05:21 -0700

    Refactored align

--- align/align.py
+++ align/align.py
@@ -1,70 +1,95 @@
-from . import align_astroalign
-from .ref_image import ref_image
-from . import check_saturation
-import inspect
+# general
+import numpy as np
+_unable = "Unable to find {} module(s); {} alignment method is disabled."
+# for astroalign method
+try:
+    import astroalign
+except ImportError:
+    print(_unable.format("astroalign", "astroalign"))
+# for skimage method
+try:
+    from skimage.feature import register_translation
+    from scipy.ndimage import fourier_shift
+except ImportError:
+    print(_unable.format("skimage or scipy", "skimage"))
+# for chi2 method
+try:
+    from image_registration import chi2_shift
+    from image_registration import fft_tools
+except ImportError:
+    print(_unable.format("image_registration", "chi2"))
+# for imreg method
+try:
+    import imreg_dft
+except ImportError:
+    print(_unable.format("imreg_dft", "imreg"))
+# types
+from numpy import ndarray
+from astropy.io.fits.hdu.image import ExtensionHDU, ImageHDU, PrimaryHDU
 
-# Updated with SDI v1.2
 
-def ALIGN():
-    current_processes = str(inspect.getouterframes(inspect.currentframe(), 2))
-    automated = True if "auto.py" in current_processes else False
-    if automated:
-        print("align.py is being ran as a subprocess of auto.py")
-        location = alignment[0]
-        check = alignment[1]
-        move = alignment[2]
-    if not automated: location = input("-> Enter path to data directory: ")
-    sat = check_saturation.check_saturate(location)
-    if sat == 0:
-        ref_image(location)
-        align_astroalign.align2(location)
+_hdu_types = (ExtensionHDU, ImageHDU, PrimaryHDU)
+
+
+def _to_np(np_or_hdu):
+    """
+    Returns a numpy array or the data; or throws an error.
+    Only for internal use within align.py
+    """
+    if isinstance(np_or_hdu, np.ndarray):
+        return np_or_hdu
+    elif isinstance(np_or_hdu, _hdu_types):
+        return np_or_hdu.data
     else:
-        if not automated: check = input("-> Saturated images found, continue image alignment? (y/n) (leave blank for default = y): ")
-        if ((check == 'y') or (check == '')):
-            if not automated: move = input("-> Move saturated images to SDI archives before continuing? (y/n) (leave blank for default = n): ")
-            if move == 'y':
-                check_saturation.move_arch(sat)
-                ref_image(location)
-                align_astroalign.align2(location)
-            elif ((move == 'n') or (move == '')):
-                ref_image(location)
-                align_astroalign.align2(location)
-            else:
-                print("-> Unknown input: must be y or n")
-        elif check =='n':
-            pass
-        else:
-            print("-> Unknown input: must be y or n")
+        raise TypeError("Cannot align to unexpected type {}".format( \
+                        type(np_or_hdu)))
 
 
-
-if __name__ == '__main__':
-    current_processes = str(inspect.getouterframes(inspect.currentframe(), 2))
-    automated = True if "auto.py" in current_processes else False
-    if automated:
-        print("align.py is being ran as a subprocess of auto.py")
-        location = alignment[0]
-        check = alignment[1]
-        move = alignment[2]
-    if not automated: location = input("-> Enter path to data directory: ")
-    sat = check_saturation.check_saturate(location)
-    if sat == 0:
-        ref_image(location)
-        align_astroalign.align2(location)
+def align(source_s, reference, method="astroalign"):
+    """
+    Aligns the source astronomical image(s) to the reference astronomical image
+    Arguments:
+        source_s -- the image(s) to align; fitsio HDU object, numpy array,
+            or a list of either one of the above
+        reference -- the image against which to align the source image;
+            fitsio HDU object or numpy array
+    Keyword Arguments:
+        method -- the library to use to align the images. options are:
+            astroalign (default), skimage, imreg, skimage, chi2
+    Return:
+        returns a transformed copy of the source image[s] in the same data type
+        which was passed in
+    """
+    np_ref = _to_np(reference)
+    # make sure that we can handle source as a list
+    sources = []
+    outputs = []
+    if isinstance(source_s, list):
+        sources = source_s
     else:
-        if not automated: check = input("-> Saturated images found, continue image alignment? (y/n) (leave blank for default = y): ")
-        if ((check == 'y') or (check == '')):
-            if not automated: move = input("-> Move saturated images to SDI archives before continuing? (y/n) (leave blank for default = n): ")
-            if move == 'y':
-                check_saturation.move_arch(sat)
-                ref_image(location)
-                align_astroalign.align2(location)
-            elif ((move == 'n') or (move == '')):
-                ref_image(location)
-                align_astroalign.align2(location)
-            else:
-                print("-> Unknown input: must be y or n")
-        elif check =='n':
-            pass
+        sources.append(source_s)
+
+    for source in sources:
+        np_src = _to_np(source)
+        # possibly unneccessary but unsure about scoping
+        output = np.array([])
+
+        if method == "astroalign":
+            output = astroalign.register(np_src, np_ref)[0]
+        elif method == "skimage":
+            shift, error, diffphase = register_translation(np_ref, np_src, 100)
+            output_fft = fourier_shift(np.fft.fftn(np_src), shift)
+            output = np.fft.ifftn(output_fft)
+        elif method == "chi2":
+            dx, dy, edx, edy = chi2_shift(np_ref, np_src, upsample_factor='auto')
+            output = fft_tools.shift.shiftnd(data, (-dx, -dy))
+        elif method == "imreg":
+            output = imreg_dft.similarity(np_ref, np_src)["timg"]
         else:
-            print("-> Unknown input: must be y or n")
+            raise ValueError("Unexpected alignment method {}!".format(method))
+
+        if isinstance(source, _hdu_types):
+            output = fits.PrimaryHDU(output, source.header)
+        outputs.append(output)
+
+    return outputs if len(outputs) > 1 else outputs[0]
