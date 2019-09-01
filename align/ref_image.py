1328f75ae127327ac9be258b03a03bd7786d4f90
Varun Iyer <varun_iyer@posteo.net>
2019-09-01 15:06:02 -0700

    Finished ref_image

--- align/ref_image.py
+++ align/ref_image.py
@@ -1,32 +1,24 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun  4 12:58:10 2018
-
-@author: andrew
-"""
-
 import glob
 import numpy as np
 from astropy.io import fits
 import os
+from .align import _to_np
 
-#%%
-#rename lowest noise image to be the reference image
-def ref_image(location):
-    noise = []
-    ref = glob.glob(location + "/*_ref_A_.fits")
-    length = len(location) + 1
-    if ref == []:
-        print("\n-> Selecting reference image...")
-        images = glob.glob(location + "/*.fits")
-        for i in images:
-            hdu = fits.open(i)
-            data = hdu[0].data
-            noise.append(np.mean(data))
-        im = images[np.argmin(noise)]
-        reference = location + "/" + im[length:-8] + "_ref_A_.fits"
-        os.system("mv %s %s" % (im, reference))
-        print("-> designated %s as the reference image in this directory\n" % (im))
-    else:
-        print("-> reference image already exists in this directory\n")
+ 
+def ref_image(fits_list):
+    """
+    Given a list-like set of fits data, ref_image finds the one most suitable
+    to be used as a reference image.
+    Arguments
+    fits_list -- list of array-likes or fitsdata
+    Returns
+    A reference to the most suitable array
+    """
+    best = fits_list[0]
+    best_mean = np.mean(best)
+    for data in fits_list:
+        data_mean = np.mean(_to_np(data))
+        if data_mean < best_mean:
+            best = data
+            best_mean = data_mean
+    return best
