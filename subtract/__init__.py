c82ff37a9bb4fb4bd526e65dd71199e8dd30fe66
Varun Iyer <varun_iyer@protonmail.com>
2019-09-04 15:58:38 -0700

    Added docstring

--- subtract/__init__.py
+++ subtract/__init__.py
@@ -1,1 +1,6 @@
-from .subtract import subtrac
+"""
+Contains tools from subtracting astronomical images.
+METHODS
+    subtract -- subtracts a template from an image or set of images
+"""
+from .subtract import subtract
