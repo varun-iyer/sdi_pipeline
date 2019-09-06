1856f2c2c178d1b6990edf99083fca9b0713a6cf
Varun Iyer <varun_iyer@posteo.net>
2019-09-06 15:44:14 -0700

    Updated main for new extract

--- __main__.py
+++ __main__.py
@@ -5,17 +5,19 @@ from .align import align
 from .combine import combine
 from .subtract import subtract
 from .extract import extract
+import pickle
 
 file_list = argv[1:]
 science_images = [fits.open(f)["SCI"] for f in file_list]
+residuals = [fits.open(f)["SCI"] for f in glob("./*.fits")]
 aligned = align(science_images)
 print("Finished alignment")
 template = combine(aligned)
 print("Finished combine")
 residuals = subtract(aligned, template)
 print("Finished subtract")
-print(len(science_images))
-print(len(residuals))
-sources = extract(science_images, residuals)
+for i, r in enumerate(residuals):
+    r.writeto("./{}residual.fits".format(i))
+sources = extract(residuals)
+pickle.dump(sources, open("srcs.pkl", "wb"))
 print("Finished extract")
-print(sources)
