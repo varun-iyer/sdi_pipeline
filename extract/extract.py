170af827a88732a2bba0447704db5797ee14f8f9
Varun Iyer <varun_iyer@protonmail.com>
2019-09-05 23:27:20 -0700

    Finished extract

--- extract/extract.py
+++ extract/extract.py
@@ -1,42 +1,79 @@
-from . import sex
-from . import psf
-import glob
-import inspect
+"""
+Extracts variable sources
+History:
+    Created by 2019-09-05
+        Varun Iyer <varun_iyer@ucsb.edu>
+"""
+from subprocess import check_output
+from astropy.io import fits
+import config
 
-# Updated with SDI v1.2
 
-def EXTRACT():
-    current_processes = str(inspect.getouterframes(inspect.currentframe(), 2))
-    automated = True if "auto.py" in current_processes else False
-    if automated:
-        print("extract.py is being ran as a subprocess of auto.py")
-        path = extraction[0]
-    if not automated: path = input("-> Enter path to target's exposure time directory: ")
-    images = glob.glob(path + '/data/*.fits')
-    psf_data = glob.glob(path + '/psf/*')
-    if len(psf_data) == 3*len(images):
-        sex.sextractor(path)
-        sex.src_filter(path)
-    else:
-        sex.sextractor_psf(path)
-        psf.psfex(path)
-        sex.sextractor(path)
-        sex.src_filter(path)
+def _ex_sci_conf(conf_name, cat_name):
+    """
+    Sets parameters to extract sources from science images
+    Only for internal use in extract
+    """
+    sex_conf = config.Sextractor(config.SEX_DEFAULT)
+    sex_conf["CATALOG_TYPE"] = "FITS_LDAC"
+    sex_conf["CATALOG_NAME"] = cat_name
+    sex_conf["PARAMETERS_NAME"] = config.PSFEX_PARAMS_PATH
+    sex_conf["FILTER_NAME"] = config.DEFAULT_CONV_PATH
+    sex_conf.write(conf_name)
+     
+      
+def _psfex_conf(conf_name):
+    """
+    Sets psf_dir to /tmp
+    """
+    psf_conf = config.Sextractor(config.PSFEX_DEFAULT)
+    sex_conf["PSF_DIR"] = config.TMPDIR
+    sex_conf.write(conf_name)
+     
+      
+def _sex_psf_conf(conf_name):
+    """
+    Sets psf_dir to /tmp
+    """
+    sex_conf = config.Sextractor(config.SEX_DEFAULT)
+    sex_conf["CATALOG_TYPE"] = "ASCII_HEAD"
+    sex_conf["CATALOG_NAME"] = "STDOUT"
+    sex_conf["PARAMETERS_NAME"] = config.PSFEX_PARAMS_PATH
+    sex_conf["FILTER_NAME"] = config.DEFAULT_CONV_PATH
+    sex_conf["PSF_NAME"] = config.DEFAULT_CONV_PATH
+    sex_conf.write(conf_name)
 
-if __name__ == '__main__':
-    current_processes = str(inspect.getouterframes(inspect.currentframe(), 2))
-    automated = True if "auto.py" in current_processes else False
-    if automated:
-        print("extract.py is being ran as a subprocess of auto.py")
-        path = extraction[0]
-    if not automated: path = input("-> Enter path to target's exposure time directory: ")
-    images = glob.glob(path + '/data/*.fits')
-    psf_data = glob.glob(path + '/psf/*')
-    if len(psf_data) == 3*len(images):
-        sex.sextractor(path)
-        sex.src_filter(path)
-    else:
-        sex.sextractor_psf(path)
-        psf.psfex(path)
-        sex.sextractor(path)
-        sex.src_filter(path)
+
+def extract(science, residual):
+    """
+    Uses sextractor to determine variable light sources in astronomical data
+    Arguments:
+        science -- HDU containing science image
+        residual -- HDU containing residual image
+    Returns:
+        A list of Source objects representing the location and various metrics
+            of detected variable sources
+    """
+    # Change I/O of sextractor to match our purposes
+    # TODO these are really constants
+    img = "{}/temp.fits".format(config.TMPDIR)
+    psf = "{}/temp.psf".format(config.TMPDIR)
+    cat = "{}/temp.cat".format(config.TMPDIR)
+    sex_conf = "{}/temp.sex".format(config.TMPDIR)
+    sex_psf_conf = "{}/temp_psf.sex".format(config.TMPDIR)
+    psfcfg = "{}/psfex.config".format(config.TMPDIR)
+    _ex_sci_conf(sex_conf, cat)
+    _psfex_conf(psfcfg)
+    _sex_psf_conf(sex_psf_conf, psf_file)
+
+    outputs = []
+     
+    for sci, res in zip(science, residual):
+        sci.writeto(tmpimage)
+        # TODO the original bizzarely had [0] after the filename, is that nec.?
+        check_output("sextractor {} -c {}".format(tmpimage, tmpconf)
+        check_output("psfex {} > {} -c {}".format(tmpcat, tmppsf, tmpconf)
+        sci.writeto(tmpimage)
+        sources_string = check_output("sextractor {} -c {}".format(tmpimage, tmpconf))
+        outputs.append(sources_string)
+    return outputs
