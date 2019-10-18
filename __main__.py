bd4fd3bd4f96225ad2923398f9a0db855e081748
William <ruiyang_wang@starmaker.deepspace.ucsb.edu>
2019-10-18 16:39:14 -0700

    changed subtraction method from HOTPANTS to OIS

--- __main__.py
+++ __main__.py
@@ -6,6 +6,7 @@ from .combine import combine
 from .subtract import subtract
 from .sources import extract
 import pickle
+gausslist = [{}]
 
 file_list = argv[1:]
 science_images = [fits.open(f)["SCI"] for f in file_list]
@@ -13,14 +14,11 @@ aligned = align.image(science_images)
 print("Finished alignment")
 template = combine(aligned)
 print("Finished combine")
-residuals = subtract(aligned, template)
+residuals = subtract(aligned, template, gausslist)
 print("Finished subtract")
-im_sources = extract(residuals)
-with open("sources.txt", "w") as out:
-	for sci, sourcelist in zip(science_images, im_sources):
-		out.write("TRACKNUM {}\n".format(sci.header["TRACKNUM"]))
-		out.write("-" * 80 + "\n")
-		out.write(",".join(iter(sourcelist[0].dtype.fields)) + "\n")
-		for source in sourcelist[0]:
-			out.write(",".join([str(s) for s in iter(source)]) + "\n")
-print("Finished extract")
+
+for idx, element in enumerate(residuals):
+    print(type(element))
+    hdu = fits.PrimaryHDU(element)
+    hdu.writeto('{0}subtracted.fits'.format(idx))
+
--- subtract/subtract.py
+++ subtract/subtract.py
@@ -1,74 +1,13 @@
-"""
-Subtract employs algorithms to take the difference of set of astronomical data
-from a template
-HISTORY
-    Created/Extensively Refactored 2019-09-04
-        Varun Iyer <varun_iyer@ucsb.edu>
-"""
+import ois
 import os
-from getpass import getuser
-from subprocess import check_output
 from astropy.io import fits
-from ..config import TMPDIR
-from ..common import HDU_TYPES
 
- 
-def subtract(sources, template, method="hotpants"):
-    """
-    Subtract takes the difference of a set of astronomical data from a template
-    Arguments:
-        sources -- a fits HDU, numpy array, or list of either
-        template -- a template to subtract from, fits HDU or numpy array
-    Keyword Arguments: 
-        method -- the subtraction algorithm to use; currently only 'hotpants' is
-            implemented and it is the default
-    """
-    data_list = []
-    if isinstance(sources, list):
-        data_list = sources
-    else:
-        data_list.append(sources)
-    if method != "hotpants":
-        # TODO see below
-        raise NotImplementedError("""Subtraction methods other than hotpants
-            (IBIS, AIS) are unimplemented.""")
-    tmplim = "{}/{}_tmplim.fits".format(TMPDIR, getuser())
-    inim = "{}/{}_inim.fits".format(TMPDIR, getuser())
-    try:
-        os.remove(tmplim)
-    except OSError:
-        pass
-    if isinstance(template, HDU_TYPES):
-        template.writeto(tmplim)
-    else:
-        fits.writeto(tmplim, template)
-    outputs = []
-    for data in data_list:
-        # We need to have unique filenames that are persistent in tmpdir 
-        # because fitsio uses lazy loading
-        # memmap keeping the file open might have left the fileno alone, but
-        # I didn’t want to depend on that -- Varun Iyer <varun_iyer@ucsb.edu>
-        try:
-            os.remove(inim)
-        except OSError:
-            pass
-        if isinstance(data, HDU_TYPES):
-            data.writeto(inim)
-        else:
-            fits.writeto(inim, data)
+def subtract(sources, template, gausslist):
+    output = []
 
-        outim = "{}/{}_{}_subtracted_.fits".format(TMPDIR, getuser(), data.header["TRACKNUM"])
-        try:
-            os.remove(outim)
-        except OSError:
-            pass
-        # use check_output so that it throws an error if the return code ain’t
-        # good
-        print(check_output(
-            "hotpants -inim {} -tmplim {} -outim {}".format(inim, tmplim, outim),
-            shell=True
-        ))
-        out = fits.open(outim)[0]
-        out.data = out.data.byteswap().newbyteorder()
-        outputs.append(out)
-    return outputs if isinstance(sources, list) else outputs[0]
+    if type(sources) == list:
+        for element in sources:
+            diff = ois.optimal_system(image=element.data, refimage=template, method='Alard-Lupton', gausslist=gausslist)[0]
+            output.append(diff)     
+            
+    return output
