46871e465513eba53ff0abb0158ec6c59cd2c1b3
Varun Iyer <varun_iyer@posteo.net>
2019-10-28 17:54:10 -0700

    here have some code

--- sources/db.py
+++ sources/db.py
@@ -4,23 +4,32 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Date, String, Text, Float, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, backref
 
+
 Base = declarative_base()
+
+
 class Record(Base):
     __tablename__ = "Record"
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     ra = Column(Float)
-    dec = Column(Text(255))
+    dec = Column(Float)
 
     def __repr__(self):
         return "<Record {} RA:{} DEC:{}>".format(self.id, self.ra, self.dec)
+
 
 class Source(Base):
     __tablename__ = "Source"
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     image_id = Column(Integer, ForeignKey('Image.id'))
+    time = Column(Date, ForeignKey('Image.time'))
     record_id = Column(Integer, ForeignKey('Record.id'))
     record = Column(Text(255))
 
+    def __repr__(self):
+        return "<Source {} Image:{} Record:{}>".format(self.id, self.image_id, self.record_id)
+
+ 
 class Image(Base):
     __tablename__ = "Image"
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
@@ -28,9 +37,11 @@ class Image(Base):
     time = Column(Date, unique=True)
     lcoid = Column(Integer, unique=True)
 
+    def __repr__(self):
+        return "<Image {} Time:{} Path:{}>".format(self.id, self.time, self.path)
 
-if __name__ == '__main__':
-    print('running sqlalchemy ' + sqlalchemy.__version__)
+
+def create_session():
     engine = create_engine(r'sqlite:///sdi.db', echo=True) #connect to database
     Base.metadata.create_all(engine) #Lets create the actual sqlite database and schema!
-    session = sessionmaker(bind=engine)()
+    return sessionmaker(bind=engine)()
---
+++ sources/record.py
@@ -0,0 +1,18 @@
+import db
+import sep
+
+
+def record(image):
+    """
+    only works with lco shit for now
+    """
+    # bkg = sep.background(image.data)
+    # recarray = sep.extract(image.data - bkg.back(), bkg.globalrms * 3.0)
+    session = db.create_session()
+    cat = image["CAT"]
+    sci = image["SCI"]
+    for source in cat.data:
+        r = db.Record(ra=source["ra"], dec=source["dec"]
+        session.add(r)
+    session.commit()
+
