9b9ca09e8b073156fa82d786f3745d547c2a672a
langeleri <63379377+langeleri@users.noreply.github.com>
2020-09-06 21:38:06 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -9,8 +9,7 @@ import pickle
 import click
 import os
 file_list = list()
-save_file = ('resduals.fits.fz', 'extra')
-
+save_file = ("{spath}residual{index}{original}")
 
 @click.command()
 @click.option('-s', is_flag=True)
@@ -19,6 +18,7 @@ save_file = ('resduals.fits.fz', 'extra')
 def run(s, path, save_path):
     if s: 
         print("residuals will be saved")
+        save_base = os.path.basename(glob("{}*.fz".format(path))[0]))
         for image in glob("{}*.fz".format(path)):
             file_list.append(image)
             save = True
@@ -33,13 +33,11 @@ def run(s, path, save_path):
     print("Finished combine")
     residuals = subtract(aligned, template)
     print("Finished subtract")
-    i = int(-1)
     if bool(save) == True:
         print("Saving Residuals")
         ri = [r[0] for r in residuals]
-        for p in ri:
-            i += 1
-            fits.PrimaryHDU(p).writeto("{spath}residual{count}{original}".format(spath = list(save_path)[0], count = str(i), original = os.path.basename(glob("{}*.fz".format(path))[0])))
+        for count, p in enumerate(ri):
+            fits.PrimaryHDU(p).writeto(save_file.format(spath = list(save_path)[0], index = count, original = save_base)
     im_sources = extract([res[0] for res in residuals])
     print("Finished extract")
     pickle.dump(im_sources, open("transient_candidates.pkl","wb"))
