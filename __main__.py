bc8490a38e8945ba03fc24e41670e4aaaf1f7df0
Varun Iyer <varun_iyer@posteo.net>
2020-09-17 13:31:40 -0700

    Minor changes + path bug fix to the QoL update

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 from astropy.io import fits
-from sys import argv
+from sys import argv, exit
 from glob import glob
 from .align import align
 from .combine import combine
@@ -16,16 +16,17 @@ save_file = ("{spath}residual{index}{original}")
 @click.argument('path')
 @click.argument('save_path', nargs= -1) #nargs= -1 allows this argument to be optional but saves value as tuple
 def run(s, path, save_path):
+    save = False
+    if path[-1] != "/":
+        print("Please give the path to the files WITHOUT '*.fz' at the end.")
+        print("Aborted!")
+        exit(0)
     if s: 
         print("residuals will be saved")
         save_base = os.path.basename(glob("{}*.fz".format(path))[0])
-        for image in glob("{}*.fz".format(path)):
-            file_list.append(image)
-            save = True
-    else: 
-        for image in glob("{}*.fz".format(path)):
-            file_list.append(image)
-            save = False
+        save = True
+    for image in glob("{}*.fz".format(path)):
+        file_list.append(image)
     science_images = [fits.open(f)["SCI"] for f in file_list]
     aligned = align.image(science_images)
     print("Finished alignment")
@@ -33,9 +34,10 @@ def run(s, path, save_path):
     print("Finished combine")
     residuals = subtract(aligned, template)
     print("Finished subtract")
-    if bool(save) == True:
+    if save:
         print("Saving Residuals")
         ri = [r[0] for r in residuals]
+        save_path = os.path.expanduser(save_path)
         for count, p in enumerate(ri):
             fits.PrimaryHDU(p).writeto(save_file.format(spath = list(save_path)[0], index = count, original = save_base))
     im_sources = extract([res[0] for res in residuals])
