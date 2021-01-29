03b018accb0221b4ad2d2bf6bebfc510a950702f
Varun Iyer <varun_iyer@posteo.net>
2021-01-29 11:28:20 -0800

    Tried modifications of setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="sdi",
-    version="0.90",
+    version="0.91",
     py_modules=["sdi", "openfits", "__main__"],
+    packages=find_packages(include=["openfits", "__main__"]),
     include_package_data=True,
     install_requires=["click"],
     entry_points="""
