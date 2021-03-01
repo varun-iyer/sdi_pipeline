c81d5f8fe2aeb980c9fa97bc1158fd104cf75303
Varun Iyer <varun_iyer@posteo.net>
2021-03-01 11:28:47 -0800

    Added items to setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="sdi",
-    version="0.91",
-    py_modules=["sdi", "openfits"],
-    packages=find_packages(include=["openfits"]),
+    version="0.99",
+    py_modules=["sdi"],
+    # packages=find_packages(include=["openfits"]),
     include_package_data=True,
-    install_requires=["click"],
+    install_requires=["click", "astropy", "photutils", "ois", "pyds9"],
     entry_points="""
         [console_scripts]
         sdi=sdi:cli
