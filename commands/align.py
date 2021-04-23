27b9689b8cde84fb24c87978ea40e4285a22152d
Varun Iyer <varun_iyer@posteo.net>
2021-04-22 23:38:16 -0700

    Changed scripts import to _scripts

--- commands/align.py
+++ commands/align.py
@@ -12,7 +12,7 @@ import numpy as np
 from astropy.io.fits import PrimaryHDU, HDUList
 #from sources import Source
 import astroalign
-from scripts import snr
+from _scripts import snr
 
 def align(hduls, name="SCI", reference=None):
     """ 
