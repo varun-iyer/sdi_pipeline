a7f6da07f6b4a2dcb7b6c69026c9a4f13ac8cb14
Varun Iyer <varun_iyer@posteo.net>
2021-04-23 16:55:25 -0700

    extract is completed on cli and on import

--- sdi/extract.py
+++ sdi/extract.py
@@ -6,8 +6,10 @@ History:
     Refactored for 2.0.0 on 2021-04-23
         Varun Iyer <varun_iyer@ucsb.edu>
 """
-import sep
+import click
 from astropy.io import fits
+import sep
+from . import _cli as cli
 
 
 def extract(hduls, stddev_thresh=3.0, read_ext=0, write_ext="XRT"):
@@ -38,10 +40,34 @@ def extract(hduls, stddev_thresh=3.0, read_ext=0, write_ext="XRT"):
         header = fits.Header([fits.Card("HISTORY", "Extracted by sep.")])
         try:
             extver = int(write_ext[1])
-            name = read_ext[0]
+            extname = write_ext[0]
         except (ValueError, TypeError):
             pass
         hdul.append(fits.BinTableHDU(data=sources, header=header,
                                      name=extname, ver=extver))
 
     return hduls
+
+@cli.cli.command("extract")
+@click.option("-t", "--threshold", default=3.0,
+              help="A threshold value to use for source extraction in terms of"
+              "the number of stddevs above the background noise.", type=float)
+@click.option("-r", "--read_ext", default=0,
+              help="An index number or ext name that identifies the data in"
+              "input hduls that you want source extraction for. For LCO, this "
+              "is 0 or SCI.")
+@click.option("-w", "--write_ext", default=("XRT", 1),
+              help="An extension name and extension version that will identify"
+              "the HDUL that the resulting BinTable gets written to. Default"
+              "is `XRT 1`", type=(str, int))
+
+@cli.operator
+def extract_cmd(hduls, threshold, read_ext, write_ext):
+    """
+    Uses sep to find sources in ImageHDU data.
+    """
+    try:
+        read_ext = int(read_ext)
+    except ValueError:
+        pass
+    return extract(hduls, threshold, read_ext, write_ext)
